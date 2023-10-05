a71d3f6d6b51e9b0fb612e8e84a33571f8d2dd51
Carina Cordes <carina.cordes@ksb-sg.ch>
2023-10-05 12:11:03 +0200

    check choice

--- TicTacToe.c
+++ TicTacToe.c
@@ -15,5 +15,34 @@ int main(){
         player = (player % 2) ? 1 : 2;
         printf("Player %d, enter the choice : ",player);
         scanf("%d", &choice);
+
+        if(choice == 1 && sqaure[1] == "1")
+            sqaure[1] = mark;
+        else if(choice == 2 && square[2] == "2")
+            sqaure[2] = mark;
+        else if(choice == 3 && square[3] == "3")
+            sqaure[3] = mark;
+        else if(choice == 4 && square[4] == "4")
+            sqaure[4] = mark;
+        else if(choice == 5 && square[5] == "5")
+            sqaure[5] = mark;
+        else if(choice == 6 && square[6] == "6")
+            sqaure[6] = mark;
+        else if(choice == 7 && square[7] == "7")
+            sqaure[7] = mark;
+        else if(choice == 8 && square[8] == "8")
+            sqaure[8] = mark;
+        else if(choice == 9 && square[9] == "9")
+            sqaure[9] = mark;
+
+        else{
+            printf("Invalid option !");
+            player--;
+            getch();
+        }
+
+
+
+
     }while(i == -1);
 }
