04bf303df0d14d788105afb045ce80d49cb6d4cb
Carina Cordes <carina.cordes@ksb-sg.ch>
2023-10-05 11:58:11 +0200

    player chosen

--- TicTacToe.c
+++ TicTacToe.c
@@ -4,3 +4,16 @@
 #include <stdlib.h>
 
 char sqaure[10] = {"0", "1,", "2", "3", "4", "5", "6", "7", "8", "9"};
+int checkWin();
+void drawBoard();
+
+int main(){
+    int player = 1, i, choice; //i tracks the progress
+    char mark; // X,O
+    do{
+        drawBoard();
+        player = (player % 2) ? 1 : 2;
+        printf("Player %d, enter the choice : ",player);
+        scanf("%d", &choice);
+    }while(i == -1);
+}
